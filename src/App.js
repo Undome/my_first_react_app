54593d61178a9ba7fb127acdb9d33f385fa64634
Ark <arkdan@gmail.com>
2023-11-15 11:37:59 -0600

    Creación del CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import { useEffect } from 'react';
+
+import './App.css';
+import SearchIcon from './search.svg';
+
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=fb19df8c';
 
 const App = () => {
+
+const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+}
+
+useEffect(() => {
+ searchMovies('Spiderman');
+}, []);
+
     return (
-        <h1>App</h1>
+        <div className='app'>
+            <h1>MovieLand</h1>
+
+            <div className='search'>
+                <input 
+                    placeholder='Search for Movies'
+                    value='Superman'
+                    onChange={() => {}}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => {}}
+                />
+            </div>
+
+            <div className='container'>
+
+            </div>
+        </div>
     )
 }
 
 export default App;
+
