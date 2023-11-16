4b6d3aad96169894a8d3aef485ac2c0185b6a4ae
Ark <arkdan@gmail.com>
2023-11-15 22:46:41 -0600

    implementación de contenedor

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=fb19df8c';
 
+const movie1 = {
+        "Title": "Miles Morales Ultimate Spiderman",
+        "Year": "2021",
+        "imdbID": "tt14311386",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNmMzODkwNDktMTkyMy00MmU5LWE4MGMtYzIzZjdjNmJiZDRiXkEyXkFqcGdeQXVyNDU1NDQ0NzE@._V1_SX300.jpg"
+}
+
 const App = () => {
 
 const searchMovies = async (title) => {
@@ -36,7 +44,11 @@ useEffect(() => {
             </div>
 
             <div className='container'>
-
+                <div className='movie'>
+                    <div>
+                        
+                    </div>
+                </div>
             </div>
         </div>
     )
