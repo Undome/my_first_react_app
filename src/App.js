f7da474ec42a0341f79dbb97735fe6f0993fa995
Ark <arkdan@gmail.com>
2023-11-28 16:27:00 -0600

    Implementación de busqueda y tarjestas de pelicula

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+import MovieCard from './MovieCard';
 
 import './App.css';
 import SearchIcon from './search.svg';
@@ -14,44 +16,49 @@ const movie1 = {
 }
 
 const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
-const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
-    const data = await response.json();
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
 
-    console.log(data.Search);
-}
+        setMovies(data.Search);
+    }
 
-useEffect(() => {
- searchMovies('Spiderman');
-}, []);
+    useEffect(() => {
+    searchMovies('Spiderman');
+    }, []);
 
-    return (
-        <div className='app'>
-            <h1>MovieLand</h1>
+        return (
+            <div className='app'>
+                <h1>MovieLand</h1>
 
-            <div className='search'>
-                <input 
-                    placeholder='Search for Movies'
-                    value='Superman'
-                    onChange={() => {}}
-                />
-                <img
-                    src={SearchIcon}
-                    alt="search"
-                    onClick={() => {}}
-                />
-            </div>
-
-            <div className='container'>
-                <div className='movie'>
-                    <div>
-                        
-                    </div>
+                <div className='search'>
+                    <input 
+                        placeholder='Search for Movies'
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                    <img
+                        src={SearchIcon}
+                        alt="search"
+                        onClick={() => searchMovies(searchTerm)}
+                    />
                 </div>
+
+                {movies.length > 0
+                    ? (
+                        <div className='container'>
+                            {movies.map((movie) => (<MovieCard movie={movie} />))}
+                        </div>
+                    ) : (
+                        <div className='empty'>
+                            <h2>No movies found</h2>
+                        </div>
+                    )}
             </div>
-        </div>
-    )
+        )
 }
 
 export default App;
